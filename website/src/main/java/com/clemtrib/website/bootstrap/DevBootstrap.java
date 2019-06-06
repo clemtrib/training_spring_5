03c329fba555c46ddb8423e4f8b71f432c48837c
Clement TRIBOUILLARD <clement.tribouillard@siicanada.com>
2019-06-06 16:59:44 -0400

    add Publisher entity

--- website/src/main/java/com/clemtrib/website/bootstrap/DevBootstrap.java
+++ website/src/main/java/com/clemtrib/website/bootstrap/DevBootstrap.java
@@ -6,33 +6,42 @@ import org.springframework.stereotype.Component;
 
 import com.clemtrib.website.model.Author;
 import com.clemtrib.website.model.Book;
+import com.clemtrib.website.model.Publisher;
 import com.clemtrib.website.repositories.AuthorRepository;
 import com.clemtrib.website.repositories.BookRepository;
+import com.clemtrib.website.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 	
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 	
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	public void initData() {
+		
+		final Publisher publisher = new Publisher("Harper Collins", "Albertville, France");
+		
+		publisherRepository.save(publisher);
 
 		final Author author1 = new Author("Clement", "Tribouillard");
-		final Book book1 = new Book("Les imposteurs", "4376", "Harper Collins");
+		final Book book1 = new Book("Les imposteurs", "4376", publisher);
 		author1.getBooks().add(book1);
 		book1.getAuthors().add(author1);
 		
 		authorRepository.save(author1);
 		bookRepository.save(book1);
-		
 		final Author author2 = new Author("Louis", "Antoine");
-		final Book book2 = new Book("Mile End", "4377", "Pow Pow");
-		author2.getBooks().add(book2);	
+		final Book book2 = new Book("Mile End", "4377", publisher);
+		
+		author2.getBooks().add(book2);
+		book2.getAuthors().add(author2);
 		
 		authorRepository.save(author2);
 		bookRepository.save(book2);
---
+++ website/src/main/java/com/clemtrib/website/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.clemtrib.website.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.clemtrib.website.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
